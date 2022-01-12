9635ecff8d4fcaec736a6ede8133ac4597edccce
naoutix <jleo.dary@gmail.com>
2022-01-12 15:24:29 +0100

    maj

--- Collage_par_gradient/yay.m
+++
@@ -1,9 +0,0 @@
-close all
-home
-domain_bounds=[50,50];
-data_points=[20 20;20 30;30 20;30 30];
-%another possible example
-%punto_dato=[25 25];
-use_plot=1;
-T=fast_marching(data_points,domain_bounds,use_plot);
-mesh(T);
